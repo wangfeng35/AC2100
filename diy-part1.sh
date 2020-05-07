6857e58fc357aaafb5030e672438d29fb5b61bd5
morooi <morooiu@gmail.com>
2020-05-08 01:40:13 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,4 @@
 sed -i 's/\"#src-git\"/\"src-git\"/g' ./feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
